06b334981ab60bb272b3be532243cc1678719d52
irfanvtf <irfan.fikri@virtualtechfrontier.com>
2025-07-24 15:36:39 +0800

    refactor: store all firebase config in an .env file

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,14 +1,14 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAdSfZFkzxpV8IbBX6Z1cS6jl9WrmV-ZNU",
-  authDomain: "aux-training.firebaseapp.com",
-  projectId: "aux-training",
-  storageBucket: "aux-training.firebasestorage.app",
-  messagingSenderId: "846125523335",
-  appId: "1:846125523335:web:94fee73c364bd2da943322"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
